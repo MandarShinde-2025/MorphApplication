e4ca2e68d9048c310696ace35e12a3037c9798d0
Mandar Shinde <mandar.shinde@pepcus.com>
2025-04-03 17:17:46 +0530

    Updated .yml file

--- run-morph.ps1
+++ run-morph.ps1
@@ -1,8 +1,1 @@
-# Define the path where EasyMorph is installed
-$EasyMorphPath = "Morph.exe"
-
-# Define the .morph file path (update this path)
-$MorphFilePath = "C:\Users\Mandar.Shinde\Repository\EasyMorph\employee_department_table.morph"
-
-# Run EasyMorph GUI and execute the project
-Start-Process -FilePath $EasyMorphPath -ArgumentList $MorphFilePath -Wait
+Start-Process -FilePath "Morph.exe" -ArgumentList "`"C:\Users\Mandar.Shinde\Repository\EasyMorph\employee_department_table.morph`"" -NoNewWindow -Wait
